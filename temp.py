f4d9d0dbb9ac2cab574ba078f5d8450c3c40e001
AngelFB04 <143554930+AngelFB04@users.noreply.github.com>
2023-12-06 22:47:47 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Temp.py - converts temperatures from Celsius to Fahrenheit
+Created by Angel Barrera
+Course number: CSC138-EN
+date 12-06-2023
+Lab2
+"""
+def main():
+ celsius = 100
+ fahrenheit = (9.0 / 5.0 * celsius) + 32.0
+ print('The temperature is ', fahrenheit)
+if __name__ == '__main__' :
+ main()
